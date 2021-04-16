2ba6f08750683000ec32982fd6d8970bdf65492a
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-17 01:43:05 +0300

    Lesson43 added

---
+++ 3-Javascript_Module/43-OEM_Calculater-Add_Item/app.js
@@ -0,0 +1,154 @@
+//Storage Controller
+const StorageController = (function() {
+
+})();
+
+//Product Controller
+const ProductController = (function() {
+
+    //private
+    const Product = function(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    const data = {
+        products: [],
+        selectedProduct: null,
+        totalPrice: 0
+    }
+
+    //public
+    return {
+        getProducts: function() {
+            return data.products;
+        },
+        getData: function() {
+            return data;
+        },
+        addProduct: function(name, price) {
+            let id;
+
+            if (data.products.length > 0) {
+                id = data.products[data.products.length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+
+            const newProduct = new Product(id, name, parseFloat(price));
+            data.products.push(newProduct);
+            return newProduct;
+        }
+    }
+
+})();
+
+//UI Controller
+const UIController = (function() {
+
+    const Selectors = {
+        productList: ".item-list",
+        btnAddProduct: "#btn-add-product",
+        productName: "#product-name",
+        productPrice: "#product-price",
+        productCard: "#product-card"
+    }
+
+    return {
+        createProductList: function(products) {
+            let html = '';
+
+            products.forEach(prd => {
+                html += `
+                <tr>
+                    <td>${prd.id}</td>
+                    <td>${prd.name}</td>
+                    <td>${prd.price} $</td>
+                    <td class="text-right">
+                        <button type="submit" class="btn btn-warning btn-sm">
+                            <i class="far fa-edit"></i>
+                        </button>
+                    </td>
+                </tr>`;
+            });
+            document.querySelector(Selectors.productList).innerHTML = html;
+        },
+        getSelectors: function() {
+            return Selectors;
+        },
+        addProduct: function(prd) {
+            document.querySelector(Selectors.productCard).style.display="block";
+            var item = `
+            <tr>
+                <td>${prd.id}</td>
+                <td>${prd.name}</td>
+                <td>${prd.price} $</td>
+                <td class="text-right">
+                    <button type="submit" class="btn btn-warning btn-sm">
+                        <i class="far fa-edit"></i>
+                    </button>
+                </td>
+             </tr>
+            `;
+            document.querySelector(Selectors.productList).innerHTML += item;
+        },
+        clearInputs: function() {
+            document.querySelector(Selectors.productName).value = '';
+            document.querySelector(Selectors.productPrice).value = '';
+        },
+        hideCard: function() {
+            document.querySelector(Selectors.productCard).style.display = "none";
+        }
+    }
+
+})();
+
+//App Controller
+const App = (function(ProductCtrl, UICtrl) {
+
+    const UISelectors = UIController.getSelectors();
+
+    //Load Event Listeners
+    const loadEventListeners = function() {
+
+        //add product event
+        document.querySelector(UISelectors.btnAddProduct).addEventListener('click', productAddSubmit);
+    }
+
+    const productAddSubmit = function(e) {
+
+        const productName = document.querySelector(UISelectors.productName).value;
+        const productPrice = document.querySelector(UISelectors.productPrice).value;
+
+        if (productName !== '' && productPrice !== '') {
+            //Add Product
+            const newProduct = ProductCtrl.addProduct(productName, productPrice);
+
+            //add item to list
+            UIController.addProduct(newProduct);
+
+            //clear inputs
+            UIController.clearInputs();
+        }
+
+        e.preventDefault();
+    }
+
+    return {
+        init: function() {
+            console.log('starting app...');
+            const products = ProductCtrl.getProducts();
+
+            if (products.length == 0) {
+                UICtrl.hideCard();
+            } else {
+                UICtrl.createProductList(products);   
+            }
+            loadEventListeners();
+
+        }
+    }
+
+})(ProductController, UIController);
+
+App.init();
