8bb302970115e08088a484b374c838b7595f3345
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-16 21:48:23 +0300

    Lesson40 added

---
+++ 3-Javascript_Module/40-OEM_Calculater-Creating_Modules/app.js
@@ -0,0 +1,19 @@
+//Storage Controller
+const StorageController(function(){
+
+})();
+
+//Product Controller
+const ProductController=(function(){
+
+})();
+
+//UI Controller
+const UIController=(function(){
+
+})();
+
+//App Controller
+const AppController=(function(){
+
+})();
