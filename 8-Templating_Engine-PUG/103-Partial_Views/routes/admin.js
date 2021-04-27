d65a4eb6177fe7c61ec10cb134fb77dc0754fdbf
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-27 21:40:32 +0300

    Lesson103 added

---
+++ 8-Templating_Engine-PUG/103-Partial_Views/routes/admin.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const router=express.Router();
+
+const products=[
+    {name:'Samsung S8',price:4000,image:'1.jpg',description:'android 8'},
+    {name:'Samsung S7',price:3000,image:'2.jpg',description:'android 6'},
+    {name:'Samsung S6',price:2000,image:'3.jpg',description:'android 5'},
+    {name:'Iphone 9',price:5000,image:'4.jpg',description:'ios 11'}
+];
+
+
+router.get('/add-product',(req,res,next)=>{
+    res.render('add-product',{title:'Ürün Ekleme'});
+});
+
+router.post('/add-product',(req,res,next)=>{
+    products.push({name:req.body.name,price:req.body.price,image:req.body.image,description:req.body.description});
+    res.redirect('/');
+});
+
+exports.routes=router;
+exports.products=products;
---
+++ 8-Templating_Engine-PUG/103-Partial_Views/routes/user.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+
+const admin=require('./admin');
+
+router.get('/',(req,res,next)=>{
+    
+    res.render('index',{title:'Anasayfa',products:admin.products});
+});
+
+module.exports=router;
