83fe8c9dfa96086bd2c264a4375ae2de199c6caf
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-27 11:31:32 +0300

    Lesson97 added

---
+++ 8-Templating_Engine-PUG/97-Installing_Pug/routes/admin.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const router=express.Router();
+const path=require('path');
+
+// /admin/add-product=>GET
+
+//Sadece localhost:3000/add-product isteğini karşılar
+router.get('/add-product',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+});
+
+// /admin/add-product=>POST
+
+//Buradaki yapıya yukarıdaki form action kısmındaki değer ile erişiliyor.
+router.post('/add-product',(req,res,next)=>{
+    console.log(req.body); //bu şekilde undefined olarak gelir body-parser eklenmeli
+    res.redirect('/');
+});
+
+module.exports=router;
