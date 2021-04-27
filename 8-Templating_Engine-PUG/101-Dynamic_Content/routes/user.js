8b4dded5ddac423b7c1155f9a70fde9ad56c5948
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-27 20:59:37 +0300

    user.js updated

--- 8-Templating_Engine-PUG/101-Dynamic_Content/routes/user.js
+++ 8-Templating_Engine-PUG/101-Dynamic_Content/routes/user.js
@@ -4,10 +4,10 @@ const path=require('path');
 
 router.get('/',(req,res,next)=>{
     const products=[
-        {name:'Samsung S8',price:3000,image:'1.jpg',description:'android 8'},
-        {name:'Samsung S7',price:3000,image:'1.jpg',description:'android 6'},
-        {name:'Samsung S6',price:3000,image:'1.jpg',description:'android 5'},
-        {name:'Iphone 9',price:3000,image:'1.jpg',description:'ios 11'}
+        {name:'Samsung S8',price:4000,image:'1.jpg',description:'android 8'},
+        {name:'Samsung S7',price:3000,image:'2.jpg',description:'android 6'},
+        {name:'Samsung S6',price:2000,image:'3.jpg',description:'android 5'},
+        {name:'Iphone 9',price:5000,image:'4.jpg',description:'ios 11'}
     ];
 
     res.render('index',{title:'Anasayfa',products:products});
