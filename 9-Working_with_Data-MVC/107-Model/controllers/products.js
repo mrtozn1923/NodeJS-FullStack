7a228c31b4b3898eb824810fa49fb6b6e04f2dcf
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-28 18:15:44 +0300

    Lesson107 added

---
+++ 9-Working_with_Data-MVC/107-Model/controllers/products.js
@@ -0,0 +1,25 @@
+const Product=require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+    const products=Product.getAll();
+    res.render('index', {
+        title: 'Anasayfa',
+        products: products,
+        path: '/'
+    });
+}
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+        title: 'Ürün Ekleme',
+        path: '/admin/add-product'
+    });
+}
+exports.postAddProduct = (req, res, next) => {
+    const product=new Product(
+        req.body.name,
+        req.body.price,
+        req.body.imageUrl,
+        req.body.description);
+    product.saveProduct();
+    res.redirect('/');
+}
