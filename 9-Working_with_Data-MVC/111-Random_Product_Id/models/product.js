1090477003435506a692a6a27d9cdd91ebe79c23
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-28 22:10:39 +0300

    Lesson111 added

---
+++ 9-Working_with_Data-MVC/111-Random_Product_Id/models/product.js
@@ -0,0 +1,23 @@
+const products = [
+    {id:"12345",name:'Iphone 6S',price:'4000',imageUrl:'1.jpg',description:'yüksek performans'},
+    {id:"12346",name:'Iphone 6',price:'3500',imageUrl:'2.jpg',description:'kullanıcı dostu'},
+    {id:"12347",name:'Samsung S6',price:'3000',imageUrl:'3.jpg',description:'yüksek çözünürlük'},
+    {id:"12348",name:'Samsung S7',price:'4000',imageUrl:'4.jpg',description:'yüksek kalite'}
+];
+
+module.exports=class Product{
+    constructor(name,price,imageUrl,description){
+        this.id=Math.floor(Math.random()*99999)+1;
+        this.name=name;
+        this.price=price;
+        this.imageUrl=imageUrl;
+        this.description=description;
+    }
+    saveProduct(){
+        products.push(this);
+    }
+
+    static getAll(){
+        return products;
+    }
+}
---
+++ 9-Working_with_Data-MVC/111-Random_Product_Id/routes/shop.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+
+const shopController=require('../controllers/shop');
+
+router.get('/',shopController.getIndex);
+router.get('/products',shopController.getProducts);
+router.get('/details',shopController.getProductDetails);
+router.get('/orders',shopController.getOrders);
+router.get('/cart',shopController.getCart);
+
+module.exports=router;
