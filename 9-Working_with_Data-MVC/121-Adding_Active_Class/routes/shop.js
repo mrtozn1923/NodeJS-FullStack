800e89e641884586f1d2bb5bbde709a70bf5ac28
mrtozn1923 <mrtozn1923@gmail.com>
2021-04-29 17:28:19 +0300

    Lesson121 added

---
+++ 9-Working_with_Data-MVC/121-Adding_Active_Class/routes/shop.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const router=express.Router();
+
+const shopController=require('../controllers/shop');
+
+router.get('/',shopController.getIndex);
+
+router.get('/products',shopController.getProducts);
+
+router.get('/products/:productid',shopController.getProduct);
+
+router.get('/categories/:categoryid',shopController.getProductsByCategoryId);
+
+router.get('/details',shopController.getProductDetails);
+
+router.get('/orders',shopController.getOrders);
+
+router.get('/cart',shopController.getCart);
+
+module.exports=router;
