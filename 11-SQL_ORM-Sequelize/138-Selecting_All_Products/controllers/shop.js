f057f137b60ab0dc06af1fcbaf438ae66c490d9f
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-02 21:31:28 +0300

    Lesson138 added

---
+++ 11-SQL_ORM-Sequelize/138-Selecting_All_Products/controllers/shop.js
@@ -0,0 +1,100 @@
+const Product = require('../models/product');
+const Category = require('../models/category');
+
+exports.getIndex = (req, res, next) => {
+
+    Product.findAll()
+        .then(products => {
+            Category.findAll()
+                .then(categories=>{
+                    res.render('shop/index', {
+                        title: 'Anasayfa',
+                        products: products,
+                        categories: categories,
+                        path: '/'
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
+exports.getProducts = (req, res, next) => {
+
+    Product.findAll()
+        .then(products => {
+            Category.findAll()
+                .then(categories=>{
+                    res.render('shop/products', {
+                        title: 'Ürünler',
+                        products: products,
+                        categories: categories,
+                        path: '/'
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+exports.getProductsByCategoryId = (req, res, next) => {
+    const categoryid = req.params.categoryid;
+    Category.getAll()
+        .then((categories) => {
+            Product.getProductsByCategoryId(categoryid)
+                .then(products => {
+                    res.render('shop/products', {
+                        title: 'Ürünler',
+                        products: products[0],
+                        categories: categories[0],
+                        selectedCategory: categoryid,
+                        path: '/products'
+                    });
+                })
+                .catch((err) => {
+                    console.log(err => {
+                        console.log(err);
+                    });
+                });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+
+
+}
+exports.getProduct = (req, res, next) => {
+    const productId = req.params.productid;
+    Product.getById(productId)
+        .then((product) => {
+            res.render('shop/product-detail', {
+                title: product[0][0].name,
+                product: product[0][0],
+                path: '/products'
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+exports.getCart = (req, res, next) => {
+    res.render('shop/cart', {
+        title: 'Alışveriş Sepeti',
+        path: '/cart'
+    });
+}
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', {
+        title: 'Siparişler',
+        path: '/orders'
+    });
+}
