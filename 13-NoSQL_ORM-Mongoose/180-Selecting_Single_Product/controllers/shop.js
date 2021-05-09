49849d70b64b8a1033e2c918edc6b39e78f033b5
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-09 19:42:10 +0300

    Lesson180 aaded

---
+++ 13-NoSQL_ORM-Mongoose/180-Selecting_Single_Product/controllers/shop.js
@@ -0,0 +1,143 @@
+const Product = require('../models/product');
+const Category = require('../models/category');
+
+exports.getIndex = (req, res, next) => {
+
+    Product.find()
+        .then(products => {
+            res.render('shop/index', {
+                title: 'Alışveriş',
+                products: products,
+                path: '/'
+            });
+            // Category.findAll()
+            //     .then(categories=>{
+            //         res.render('shop/index', {
+            //             title: 'Alışveriş',
+            //             products: products,
+            //             path: '/',
+            //             categories:categories
+            //         });
+            //     })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+}
+exports.getProducts = (req, res, next) => {
+
+    Product.find()
+        .then(products => {
+            res.render('shop/products', {
+                title: 'Ürünler',
+                products: products,
+                path: '/products'
+            });
+            // Category.findAll()
+            //     .then(categories => {
+            //         res.render('shop/products', {
+            //             title: 'Ürünler',
+            //             products: products,
+            //             path: '/products',
+            //             categories: categories
+            //         });
+            //     })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+exports.getProductsByCategoryId = (req, res, next) => {
+    const categoryid = req.params.categoryid;
+    const model = [];
+
+    Category.find()
+        .then(categories => {
+            model.categories = categories;
+            return Product.findByCategoryId(categoryid);
+        })
+        .then(products => {
+            res.render('shop/products', {
+                title: 'Ürünler',
+                products: products,
+                categories: model.categories,
+                selectedCategory: categoryid,
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));
+
+}
+exports.getProduct = (req, res, next) => {
+
+    Product
+        // .findById(req.params.productid)
+        .findOne({_id:req.params.productid})
+        .then(product => {
+            res.render('shop/product-detail', {
+                title: product.name,
+                product: product,
+                path: '/products'
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+        .then(products => {
+            res.render('shop/cart', {
+                title: 'Alışveriş Sepeti',
+                path: '/cart',
+                products: products
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+exports.postCart = (req, res, next) => {
+
+    const productId = req.body.productId;
+    Product.findById(productId)
+        .then(product => {
+            return req.user.addToCart(product);
+        })
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
+}
+exports.postCartItemDelete = (req, res, next) => {
+    const productid = req.body.productid;
+
+    req.user
+        .deleteCartItem(productid)
+        .then(() => {
+            res.redirect('/cart');
+        });
+}
+
+exports.getOrders = (req, res, next) => {
+    req.user.getOrders()
+        .then(orders => {
+            res.render('shop/orders', {
+                title: 'Siparişler',
+                path: '/orders',
+                orders: orders
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+exports.postOrder = (req, res, next) => {
+    req.user
+        .addOrder()
+        .then(() => {
+            res.redirect('/cart');
+        })
+        .catch(err => console.log(err));
+}
