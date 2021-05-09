c85809d09def85c5e95afb08c18e65af3bf7f82e
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-09 21:43:51 +0300

    Lesson183 added

---
+++ 13-NoSQL_ORM-Mongoose/183-Deleting_Product/app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/errors');
+
+const User = require('./models/user');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findOne('mert')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => { console.log(err) });
+})
+
+//routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+
+app.use(errorController.get404Page);
+
+
+mongoose.connect('mongodb://localhost/node-app')
+    .then(() => {
+        console.log('connected to mongodb');
+
+        User.findOne({ name: 'mert' })
+            .then(user => {
+                if (!user) {
+                    user = new User({
+                        name: 'mert',
+                        email: 'mert@gmail.com',
+                        cart: {
+                            items: []
+                        }
+                    })
+                    return user.save();
+                }
+                return user;
+            })
+            .then(() => {
+                app.listen(3000);
+            })
+            .catch(err => console.log(err));
+    })
+    .catch(err => {
+        console.log(err);
+    })
