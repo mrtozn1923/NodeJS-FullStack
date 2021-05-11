4d3a38e5df62b01ed01fbaf7f9a486984e0cd4b0
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-11 22:44:25 +0300

    Lesson200 added

---
+++ 14-Authentication/200-Sessions/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+const adminRoutes = require('./routes/admin');
+const userRoutes = require('./routes/shop');
+const accountRoutes = require('./routes/account');
+const mongoose = require('mongoose');
+const cookieParser=require('cookie-parser');
+const session=require('express-session');
+
+const errorController = require('./controllers/errors');
+
+const User = require('./models/user');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+    secret:'keyboard cat',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:3600000 //1 hour
+    }
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findOne({name:'mert'})
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => { console.log(err) });
+})
+
+//routes
+app.use('/admin', adminRoutes);
+app.use(userRoutes);
+app.use(accountRoutes);
+
+app.use(errorController.get404Page);
+
+
+mongoose.connect('mongodb://localhost/node-app')
+    .then(() => {
+        console.log('connected to mongodb');
+
+        User.findOne({ name: 'mert' })
+            .then(user => {
+                if (!user) {
+                    user = new User({
+                        name: 'mert',
+                        email: 'mert@gmail.com',
+                        cart: {
+                            items: []
+                        }
+                    })
+                    return user.save();
+                }
+                return user;
+            })
+            .then(() => {
+                app.listen(3000);
+            })
+            .catch(err => console.log(err));
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ 14-Authentication/200-Sessions/controllers/account.js
@@ -0,0 +1,40 @@
+exports.getLogin=(req,res,next)=>{
+    res.render('account/login',{
+        path:'/login',
+        title:'Giriş Yap'
+    });
+}
+
+exports.postLogin=(req,res,next)=>{
+    const email=req.body.email;
+    const password=req.body.password;
+    if((email=='mert@gmail.com') && (password=='1234')){
+        // res.cookie('isAuthenticated',true);
+        req.session.isAuthenticated=true;
+        res.redirect('/');
+    }else{
+        res.redirect('/login');
+    }
+}
+
+exports.getRegister=(req,res,next)=>{
+    res.render('account/register',{
+        path:'/register',
+        title:'Kayıt Ol'
+    });
+}
+
+exports.postRegister=(req,res,next)=>{
+    res.redirect('/login');
+}
+
+exports.getReset=(req,res,next)=>{
+    res.render('account/reset',{
+        path:'/reset',
+        title:'Şifreyi Sıfırla'
+    });
+}
+
+exports.postReset=(req,res,next)=>{
+    res.redirect('/');
+}
