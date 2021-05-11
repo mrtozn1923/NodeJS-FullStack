2640c8fb95ecb74bd7d628b63e13b1ac6058c007
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-11 22:54:04 +0300

    Lesson201 added

---
+++ 14-Authentication/201-Store_Sessions_Database/models/category.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+categorySchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:String
+});
+
+module.exports=mongoose.model('Category',categorySchema);
