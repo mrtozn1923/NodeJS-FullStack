8863ff1e7979e4a545d55d785e249e2bc4f37439
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-14 15:47:31 +0300

    Revert "Lesson15 added"
    This reverts commit 4453f4a81e10d73f49bfab5c12f80d288a482e3c.

--- 15-Validation&Error_Handling/219-Error_Handling/middleware/authentication.js
+++
@@ -1,8 +0,0 @@
-module.exports=(req,res,next)=>{
-    console.log(req.session);
-    if(!req.session.isAuthenticated){
-        req.session.redirectTo=req.url;
-        return res.redirect('/login');
-    }
-    next();
-}
--- 15-Validation&Error_Handling/219-Error_Handling/middleware/isAdmin.js
+++
@@ -1,9 +0,0 @@
-module.exports=(req,res,next)=>{
-    if(!req.session.isAuthenticated){
-        return res.redirect('/login');
-    }
-    if(!req.user.isAdmin){
-        return res.redirect('/');
-    }
-    next();
-}
