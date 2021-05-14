3c40688352c2c5d7998609e83bb46f7d0abdbdd6
mrtozn1923 <mrtozn1923@gmail.com>
2021-05-14 15:02:26 +0300

    Lesson218 added

---
+++ 15-Validation&Error_Handling/218-Login_Validation/models/user.js
@@ -0,0 +1,95 @@
+const Product = require('./product')
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        //validator.js
+        validate:[isEmail,'Geçersiz E-posta adresi.']
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    cart: {
+        items: [{
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            }
+        }]
+    }
+});
+userSchema.methods.addToCart = function(product) {
+    const index = this.cart.items.findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+    });
+    const updatedCartItems = [...this.cart.items];
+    let itemQuantity = 1;
+    if (index >= 0) {
+        //Ürün sepette zaten var.Adet arttırılacak.
+        itemQuantity = this.cart.items[index].quantity + 1;
+        updatedCartItems[index].quantity = itemQuantity;
+    } else {
+        //Ürün daha önce sepete eklenmemiş.Yeni eklenecek.
+        updatedCartItems.push({
+            productId: product._id,
+            quantity: itemQuantity
+        });
+    }
+    this.cart = {
+        items: updatedCartItems
+    }
+    return this.save();
+}
+userSchema.methods.getCart = function() {
+    const ids = this.cart.items.map(i => {
+        return i.productId;
+    });
+    return Product
+        .find({
+            _id: {
+                $in: ids
+            }
+        })
+        .select('name price imageUrl')
+        .then(products => {
+            return products.map(p => {
+                return {
+                    name: p.name,
+                    price: p.price,
+                    imageUrl: p.imageUrl,
+                    quantity: this.cart.items.find(i => {
+                        return i.productId.toString() === p._id.toString()
+                    }).quantity
+                }
+            });
+        });
+}
+
+userSchema.methods.deleteCartItem = function(productid) {
+    const cartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productid.toString()
+    });
+    this.cart.items=cartItems;
+    return this.save();
+}
+userSchema.methods.clearCart = function() {
+    this.cart={items:[]};
+    return this.save();
+}
+module.exports = mongoose.model('User', userSchema);
---
+++ 15-Validation&Error_Handling/218-Login_Validation/utility/database.js
@@ -0,0 +1,25 @@
+const mongodb=require('mongodb');
+const MongoClient=mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect=(callback)=>{
+    MongoClient.connect('mongodb://localhost/node-app')
+       .then(client=>{
+            console.log('connected');
+            _db=client.db();
+            callback();
+       })
+       .catch(err=>{
+           console.log(err);
+           throw err;
+       })
+}
+const getdb=()=>{
+    if(_db){
+        return _db;
+    }
+    throw 'No Database';
+}
+exports.mongoConnect=mongoConnect;
+exports.getdb=getdb;
